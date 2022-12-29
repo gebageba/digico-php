a23ff53938c10bd959e441b28d5d9e0dfbf50ebf
gebageba <nagaikanae34@gmail.com>
2022-12-29 09:24:50 +0900

    :recycle: PHPのEnumを使用

--- src/DigicoResponseDetailCode.php
+++ src/DigicoResponseDetailCode.php
@@ -2,22 +2,20 @@
 
 namespace Evolu\Digico;
 
-use MyCLabs\Enum\Enum;
-
-class DigicoResponseDetailCode extends Enum
+Enum DigicoResponseDetailCode
 {
-    public const SUCCESS = '00';
-    public const NOW_MAINTENANCE = '01';
-    public const DUPLICATE_TRAN = '03';
-    public const EMPTY_GIFT_STOCK = '04';
-    public const DAILY_CAPACITY_OVER = '05';
-    public const MONTHLY_CAPACITY_OVER = '06';
-    public const RESPONSE_TIME_OUT= '09';
-    public const INVALID_FORMAT = '21';
-    public const INVALID_RESPONSE_TYPE = '23';
-    public const INVALID_GIFT_IDENTIFY_CODE = '24';
-    public const INVALID_TRADE_ID = '25';
-    public const INVALID_TIMESTAMP = '27';
-    public const INVALID_SIGNATURE = '28';
-    public const INTERNAL_ERROR = '99';
+    const SUCCESS = '00';
+    const NOW_MAINTENANCE = '01';
+    const DUPLICATE_TRAN = '03';
+    const EMPTY_GIFT_STOCK = '04';
+    const DAILY_CAPACITY_OVER = '05';
+    const MONTHLY_CAPACITY_OVER = '06';
+    const RESPONSE_TIME_OUT= '09';
+    const INVALID_FORMAT = '21';
+    const INVALID_RESPONSE_TYPE = '23';
+    const INVALID_GIFT_IDENTIFY_CODE = '24';
+    const INVALID_TRADE_ID = '25';
+    const INVALID_TIMESTAMP = '27';
+    const INVALID_SIGNATURE = '28';
+    const INTERNAL_ERROR = '99';
 }
